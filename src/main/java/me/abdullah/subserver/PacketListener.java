12a496a4ce4b1dd04d7c81bbf1e30493cbfaba8a
dedose1 <ahrasheed2004@gmail.com>
2022-02-18 20:34:49 -0600

    Cleanup

--- src/main/java/me/abdullah/subserver/PacketListener.java
+++ src/main/java/me/abdullah/subserver/PacketListener.java
@@ -6,7 +6,6 @@ import me.abdullah.subserver.net.Connection;
 import me.abdullah.subserver.net.packets.PlayerConnectPacket;
 import me.abdullah.subserver.net.packets.PlayerConnectionInfoPacket;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 
 import java.util.HashMap;
 import java.util.Map;
