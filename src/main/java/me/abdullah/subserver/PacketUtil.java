cf073e61051cdd3b0f2cca28b7f7739da5052cfe
dedose1 <ahrasheed2004@gmail.com>
2022-02-20 14:48:07 -0600

    Packet util

--- src/main/java/me/abdullah/subserver/PacketUtil.java
+++ src/main/java/me/abdullah/subserver/PacketUtil.java
@@ -1,6 +1,18 @@
 package me.abdullah.subserver;
 
-public class PacketBuilder {
+import java.lang.reflect.Array;
+import java.util.function.Function;
 
+public class PacketUtil {
 
+    // Recommend against using this for time-sensitive packets because the use of reflection
+    // can considerably impact time
+    public static <T, R> R[] toArray(T[] arr, Class<R> clazz, Function<T, R> converter){
+        R[] r = (R[]) Array.newInstance(clazz, arr.length);
+        for (int i = 0; i < arr.length; i++) {
+            r[i] = converter.apply(arr[i]);
+        }
+
+        return r;
+    }
 }
